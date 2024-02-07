f83231b5b6dc641f1292b18fd99baa6cfee91754
smashyalts <smashyalts@gmail.com>
2024-02-07 08:12:45 +0100

    Update Installer.sh (should work now)

--- Installer.sh
+++ Installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo I am not liable for any damage caused by this script, backups of files directly modified by this script will be automatically made, named FileBackup.tsx and DashboardBackup.tsx
-echo Type 1 for Server sort, 2 for Pagination, 3 for both
+echo "Type 1 for Server sort, 2 for Pagination, 3 for both"
 read -p 'Option: ' number
 
 sudo mkdir -p /etc/apt/keyrings
@@ -13,24 +12,16 @@ npm i -g yarn # Install Yarn
 
 cd /var/www/pterodactyl
 yarn # Installs panel build dependencies
-if number = 1 
-then   
- yarn add react-beautiful-dnd
- cp /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx DashboardBackup.tsx
- curl https://www.dropbox.com/scl/fi/5mdgeh0e596i6h76d6apy/DashboardContainer.tsx?rlkey=hwexoub3l63isxag31g577q71 > /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx
-fi
-elif number = 2 
-then
- cp /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
- curl https://www.dropbox.com/scl/fi/1x6ma2ww4x24v0w9equ2n/FileManagerContainer.tsx?rlkey=bvk8gj7qxqietg54gggot71dc > /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
- fi
-elif number = 3
-then 
- cp /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
- cp /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx DashboardBackup.tsx
- curl https://www.dropbox.com/scl/fi/5mdgeh0e596i6h76d6apy/DashboardContainer.tsx?rlkey=hwexoub3l63isxag31g577q71 > /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx
- curl https://www.dropbox.com/scl/fi/1x6ma2ww4x24v0w9equ2n/FileManagerContainer.tsx?rlkey=bvk8gj7qxqietg54gggot71dc > /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
- yarn add react-beautiful-dnd 
+
+if [ "$number" -eq "1" ]; then
+  yarn add react-beautiful-dnd
+  curl -L -f https://www.dropbox.com/scl/fi/5mdgeh0e596i6h76d6apy/DashboardContainer.tsx?rlkey=hwexoub3l63isxag31g577q71 > /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx
+elif [ "$number" -eq "2" ]; then
+  curl -L -f https://www.dropbox.com/scl/fi/1x6ma2ww4x24v0w9equ2n/FileManagerContainer.tsx?rlkey=bvk8gj7qxqietg54gggot71dc > /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
+elif [ "$number" -eq "3" ]; then
+  curl -L -f https://www.dropbox.com/scl/fi/5mdgeh0e596i6h76d6apy/DashboardContainer.tsx?rlkey=hwexoub3l63isxag31g577q71 > /var/www/pterodactyl/resources/scripts/components/dashboard/DashboardContainer.tsx
+  curl -L -f https://www.dropbox.com/scl/fi/1x6ma2ww4x24v0w9equ2n/FileManagerContainer.tsx?rlkey=bvk8gj7qxqietg54gggot71dc > /var/www/pterodactyl/resources/scripts/components/server/files/FileManagerContainer.tsx
+  yarn add react-beautiful-dnd
 fi
 
 yarn build:production
